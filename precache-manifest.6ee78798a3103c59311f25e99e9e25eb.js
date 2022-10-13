54974be31a43909f17de4842fba154ef5f74529f
IvanNapsterKE <nguyoivan@gmail.com>
2022-10-13 17:49:28 +0300

    Updates

---
+++ precache-manifest.6ee78798a3103c59311f25e99e9e25eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d2a9ceb9cf4603d08eaea78a3825f78",
+    "url": "/Phase-2-project/index.html"
+  },
+  {
+    "revision": "7a8e22573c2c8260da79",
+    "url": "/Phase-2-project/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "8815036729f94952dc57",
+    "url": "/Phase-2-project/static/css/main.686842f3.chunk.css"
+  },
+  {
+    "revision": "7a8e22573c2c8260da79",
+    "url": "/Phase-2-project/static/js/2.7082f3bc.chunk.js"
+  },
+  {
+    "revision": "c9c5f12326e730f6171c9e7086115877",
+    "url": "/Phase-2-project/static/js/2.7082f3bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8815036729f94952dc57",
+    "url": "/Phase-2-project/static/js/main.35345ab1.chunk.js"
+  },
+  {
+    "revision": "28f1a403f661d7f64b78",
+    "url": "/Phase-2-project/static/js/runtime-main.43399af7.js"
+  }
+]);
